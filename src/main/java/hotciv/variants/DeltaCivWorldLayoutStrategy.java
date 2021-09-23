f95fd2667054ede47fa12797ad1b4fe419c8e25e
Christian Wolter Grann <202009898@post.au.dk>
2021-09-23 12:03:47 +0200

    [ADDED] Worldmap generation for tiles, so it look the same as 36.1 in the book
    All test passed

---
+++ src/main/java/hotciv/variants/DeltaCivWorldLayoutStrategy.java
@@ -0,0 +1,66 @@
+package hotciv.variants;
+
+import hotciv.framework.GameConstants;
+import hotciv.framework.Position;
+import hotciv.framework.Tile;
+import hotciv.standard.TileImpl;
+import hotciv.standard.WorldLayoutStrategy;
+
+import java.util.HashMap;
+
+public class DeltaCivWorldLayoutStrategy implements WorldLayoutStrategy {
+    private HashMap<Position, Tile> worldLayout;
+    @Override
+    public HashMap<Position, Tile> setUpWorld() {
+        // Basically we use a 'data driven' approach - code the
+        // layout in a simple semi-visual representation, and
+        // convert it to the actual Game representation.
+        String[] layout =
+                new String[] {
+                        "...ooMooooo.....",
+                        "..ohhoooofffoo..",
+                        ".oooooMooo...oo.",
+                        ".ooMMMoooo..oooo",
+                        "...ofooohhoooo..",
+                        ".ofoofooooohhoo.",
+                        "...ooo..........",
+                        ".ooooo.ooohooM..",
+                        ".ooooo.oohooof..",
+                        "offfoooo.offoooo",
+                        "oooooooo...ooooo",
+                        ".ooMMMoooo......",
+                        "..ooooooffoooo..",
+                        "....ooooooooo...",
+                        "..ooohhoo.......",
+                        ".....ooooooooo..",
+                };
+        // Conversion...
+        worldLayout = new HashMap<>();
+        String line;
+        for ( int r = 0; r < GameConstants.WORLDSIZE; r++ ) {
+            line = layout[r];
+            for (int c = 0; c < GameConstants.WORLDSIZE; c++) {
+                char tileChar = line.charAt(c);
+                String type = "error";
+                if (tileChar == '.') {
+                    type = GameConstants.OCEANS;
+                }
+                if (tileChar == 'o') {
+                    type = GameConstants.PLAINS;
+                }
+                if (tileChar == 'M') {
+                    type = GameConstants.MOUNTAINS;
+                }
+                if (tileChar == 'f') {
+                    type = GameConstants.FOREST;
+                }
+                if (tileChar == 'h') {
+                    type = GameConstants.HILLS;
+                }
+                Position p = new Position(r, c);
+                worldLayout.put(p, new TileImpl(type));
+            }
+        }
+        return worldLayout;
+    }
+}
---
+++ src/test/java/hotciv/variants/TestDeltaCiv.java
@@ -0,0 +1,55 @@
+package hotciv.variants;
+
+import hotciv.framework.*;
+
+import hotciv.standard.GameImpl;
+import hotciv.standard.WorldLayoutStrategy;
+import org.junit.jupiter.api.*;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestDeltaCiv {
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new GameImpl(new AlphaCivAgeStrategy(), new AlphaCivWinnerStrategy(), new AlphaCivPerformUnitActionStrategy(), new DeltaCivWorldLayoutStrategy());
+    }
+
+    @Test
+    public void shouldBeOceanTileAt0_0And11_0And15_15() {
+        // Given a game with DeltaCivWorldLayoutStrategy
+        // When world is made
+        // Then there should be ocean at (0_0) and (11_0) and (15,15)
+        assertThat(game.getTileAt(new Position(11,0)).getTypeString(),is(GameConstants.OCEANS));
+        assertThat(game.getTileAt(new Position(15,15)).getTypeString(),is(GameConstants.OCEANS));
+    }
+
+    @Test
+    public void shouldBeMountainAt3_3And4_3() {
+        // Given a game with DeltaCivWorldLayoutStrategy
+        // When world is made
+        // Then there should be mountain at (3_3) and (4_3)
+        assertThat(game.getTileAt(new Position(3,3)).getTypeString(),is(GameConstants.MOUNTAINS));
+        assertThat(game.getTileAt(new Position(3,4)).getTypeString(),is(GameConstants.MOUNTAINS));
+    }
+
+    @Test
+    public void shouldBeHillAt1_3And1_4() {
+        // Given a game with DeltaCivWorldLayoutStrategy
+        // When world is made
+        // Then there should be hill at (1_3) and (1_4)
+        assertThat(game.getTileAt(new Position(1,3)).getTypeString(),is(GameConstants.HILLS));
+        assertThat(game.getTileAt(new Position(1,4)).getTypeString(),is(GameConstants.HILLS));
+    }
+
+    @Test
+    public void shouldBePlainAt0_3And4_6() {
+        // Given a game with DeltaCivWorldLayoutStrategy
+        // When world is made
+        // Then there should be plain at (0_3) and (4_6)
+        assertThat(game.getTileAt(new Position(0,3)).getTypeString(),is(GameConstants.PLAINS));
+        assertThat(game.getTileAt(new Position(4,6)).getTypeString(),is(GameConstants.PLAINS));
+    }
+}
