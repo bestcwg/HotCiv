7d0b8bfe81016b0a805719128b643ffb2460a9fb
Christian Wolter Grann <202009898@post.au.dk>
2021-11-03 15:20:49 +0100

    [ADDED] test for random generation of map
    All test passed

--- src/test/java/hotciv/variants/TestDeltaCivAdaptor.java
+++ src/test/java/hotciv/variants/TestDeltaCivAdaptor.java
@@ -8,6 +8,8 @@ import hotciv.standard.factories.DeltaCivAdapterFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,17 +17,25 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class TestDeltaCivAdaptor {
     private Game game;
 
-    @BeforeEach
-    void setUp() {
-        game = new GameImpl(new DeltaCivAdapterFactory());
-    }
-
     @Test
     public void shouldBeNoErrorTilesInWorldMap() {
+        game = new GameImpl(new DeltaCivAdapterFactory());
+
         for(int r = 0; r < 16; r++) {
             for(int c = 0; c < 16; c++) {
                 assertThat(game.getTileAt(new Position(r, c)).getTypeString(), not("error"));
             }
         }
+    }
+
+    @Test
+    public void shouldBeRandomTileOn0_0() {
+        HashSet<String> tileSet = new HashSet<>();
+
+        for (int i = 0; i < 25; i++) {
+            game = new GameImpl(new DeltaCivAdapterFactory());
+            tileSet.add(game.getTileAt(new Position(0,0)).getTypeString());
+        }
+        assertThat(tileSet.size(), not(1));
     }
 }
